e6bfb253cb8dd193e58b358228b7241950599dd6
MariyaVla <Mariya.Vlasova26@ycdsbk12.ca>
2024-11-28 09:05:04 -0500

    Testing

--- Sketch.java
+++ Sketch.java
@@ -1,3 +1,11 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class Sketch {
+    Scanner in = new Scanner(System.in);
+    
+    public void testing(){
+        System.out.println("Hello!!");
+    }
     
 }
