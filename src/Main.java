e33e70ab51e803041133002298c1c98b04d7ec74
MariyaVla <Mariya.Vlasova26@ycdsbk12.ca>
2024-12-17 08:29:08 -0500

    Moved over sketch2 to mysketch

--- src/Main.java
+++ src/Main.java
@@ -12,13 +12,13 @@ import java.util.Arrays;
 class Main {
   public static void main(String[] args) {
     //LifeLinkER: An algorithmic ER patient tracking system for live updates and priority optimization
-    String[] processingArgs = {"MySketch2"};
-	  MySketch2 MySketch2 = new MySketch2();  //comment this out to run the other sketch files
+    String[] processingArgs = {"MySketch"};
+	  MySketch MySketch = new MySketch();  //comment this out to run the other sketch files
 
 	  // Sketch1 mySketch = new Sketch1();  // uncomment this to run this sketch file
 	  // Sketch2 mySketch = new Sketch2();  // uncomment this to run this sketch file
 	  
-	  PApplet.runSketch(processingArgs, MySketch2);
+	  PApplet.runSketch(processingArgs, MySketch);
   }
   
 }
